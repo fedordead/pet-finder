c3cd005a5958ccac1ec982d0eb05345f260cbb2d
Dave Berner <davidb@campaignmonitor.com>
2016-05-15 19:01:36 +0100

    js to toggle text on form

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -1,8 +1,34 @@
-let status = document.querySelectorAll('[name="status"]');
-console.log('status ' + status);
+let petDetectrApp = petDetectrApp || {};
 
-for (let i = 0; i < status.length; i = i + 1) {
-    console.log('status: ', status[i]);
-    // status[i].addEventListener('click', hadouken, false);
-}
+petDetectrApp.formSetup = function () {
 
+    // Grab radio buttons and text nodes
+    this.petStatus = document.querySelectorAll('[name="status"]');
+    this.lastSeenText = document.querySelectorAll('.seen-found-text');
+
+    // add event listeners to radio buttons
+    for (let i = 0; i < this.petStatus.length; i = i + 1) {
+        this.petStatus[i].addEventListener('click', this.updateDateAndLocationText, true);
+    }
+};
+
+petDetectrApp.updateDateAndLocationText = function (e) {
+
+    const currentStatus = e.target.value;
+    let lastSeenValue;
+
+    if(currentStatus === 'lost') {
+        lastSeenValue = 'last seen';
+    } else if(currentStatus === 'found') {
+        lastSeenValue = 'found';
+    } else {
+        lastSeenValue = 'reported';
+    }
+
+    // updates all the text nodes
+    for (let i = 0; i < petDetectrApp.lastSeenText.length; i = i + 1) {
+        petDetectrApp.lastSeenText[i].innerHTML = lastSeenValue;
+    }
+};
+
+petDetectrApp.formSetup();
