eff36cd1cb6df02b439d5fb53869d0287460c090
Dave Berner <davidb@campaignmonitor.com>
2016-05-16 12:48:40 +0100

    updated missing brackets and missed name - added empty state

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 
 <div class="l-side-by-side l-side-by-side--gutterless">
 
-
     <aside class="l-side-by-side__item">
 
         <div class="l-sidebar">
@@ -44,15 +43,15 @@
                     <a class="c-card" href="/pet-details.php?pet=<?php echo substr($key, 1); ?>">
 
                         <div class="c-card__header">
-                            <h3 class="heading heading--main">Pet Name <?php echo $post->name; ?></h3>
+                            <h3 class="heading heading--main"><?php if($post->name) { echo $post->name; } else { echo 'Pet Name'; } ?></h3>
                         </div>
 
                         <div class="c-card__body">
                             <ul>
-                                <li>Species: <?php if($post->species) { echo $post->species; }  else echo '&mdash;' ?></li>
-                                <li>Breed: <?php if($post->breed)   { echo $post->breed; }  else echo '&mdash;' ?></li>
-                                <li>Size: <?php if($post->size)    { echo $post->size; }  else echo '&mdash;' ?></li>
-                                <li>Colour: <?php if($post->colour) { echo $post->colour; }  else echo '&mdash;' ?></li>
+                                <li>Species: <?php if($post->species) { echo $post->species; } else { echo '&mdash;'; } ?></li>
+                                <li>Breed: <?php if($post->breed)   { echo $post->breed; } else { echo '&mdash;'; } ?></li>
+                                <li>Size: <?php if($post->size)    { echo $post->size; } else { echo '&mdash;'; } ?></li>
+                                <li>Colour: <?php if($post->colour) { echo $post->colour; } else { echo '&mdash;'; } ?></li>
 
                                 <?php if($post->collar) { ?>
                                     <li>Collar: <?php echo $post->collar; ?></li>
@@ -62,8 +61,8 @@
                                     <li>Chipped: <?php echo $post->chipped; ?></li>
                                 <?php } ?>
 
-                                <li>Last seen: <?php if($post->location) { echo $post->location; }  else echo '&mdash;' ?></li>
-                                <li>At: <?php if($post->time) { echo $post->time; }  else echo '&mdash;' ?></li>
+                                <li>Last seen: <?php if($post->location) { echo $post->location; } else { echo '&mdash;'; } ?></li>
+                                <li>At: <?php if($post->time) { echo $post->time; } else { echo '&mdash;'; } ?></li>
                             </ul>
                         </div>
                     </a>
@@ -71,7 +70,8 @@
 
                 </li>
 
-            <?php   }
+            <?php
+                    }
                 }
             ?>
             </ul>
--- pet-details.php
+++ pet-details.php
@@ -7,23 +7,26 @@
 
     <main role="main" class="l-main">
 
-        <h2 class="heading heading--main h-spacing-base">{{ Lost }}: {{ Barbara }}</h2>
+    <?php
+    // get the posts
+    $pet = get_single_post();
+
+    if($pet){
+
+    ?>
+
+        <h2 class="heading heading--main h-spacing-base"><?php if($pet->status) { echo $pet->status; } else { echo 'Lost:'; } ?> <?php if($pet->name) { echo $pet->name; } else { echo 'Catty McCatFace'; } ?></h2>
 
         <img class="h-spacing-base" src="http://placekitten.com/200/300" alt="Place Kitten" />
 
         <ul class="h-spacing-base">
 
-        <?php
-            // get the posts
-            $pet = get_single_post();
 
-            if($pet){
-        ?>
 
-                <li>Species: <?php if($pet->species) { echo $pet->species; }  else echo '&mdash;' ?></li>
-                <li>Breed: <?php if($pet->breed)   { echo $pet->breed; }  else echo '&mdash;' ?></li>
-                <li>Size: <?php if($pet->size)    { echo $pet->size; }  else echo '&mdash;' ?></li>
-                <li>Colour: <?php if($pet->colour) { echo $pet->colour; }  else echo '&mdash;' ?></li>
+                <li>Species: <?php if($pet->species) { echo $pet->species; } else { echo '&mdash;'; } ?></li>
+                <li>Breed: <?php if($pet->breed)   { echo $pet->breed; } else { echo '&mdash;'; } ?></li>
+                <li>Size: <?php if($pet->size)    { echo $pet->size; } else { echo '&mdash;'; } ?></li>
+                <li>Colour: <?php if($pet->colour) { echo $pet->colour; } else { echo '&mdash;'; } ?></li>
 
                 <?php if($pet->collar) { ?>
                     <li>Collar: <?php echo $pet->collar; ?></li>
@@ -33,10 +36,10 @@
                     <li>Chipped: <?php echo $pet->chipped; ?></li>
                 <?php } ?>
 
-                <li>Last seen: <?php if($pet->location) { echo $pet->location; }  else echo '&mdash;' ?></li>
-                <li>At: <?php if($pet->time) { echo $pet->time; }  else echo '&mdash;' ?></li>
+                <li>Last seen: <?php if($pet->location) { echo $pet->location; } else { echo '&mdash;'; } ?></li>
+                <li>At: <?php if($pet->time) { echo $pet->time; } else { echo '&mdash;'; } ?></li>
 
-                <?php } ?>
+
 
             </ul>
 
@@ -48,7 +51,13 @@
             <p>{{ davidajberner@gmail.com }}</p>
         </address>
 
+        <?php } else { ?>
 
+            <h2 class="heading heading--main h-spacing-base">Awkward...</h2>
+
+            <p>Seems we've lost this pet from our records.  Double lost :S</p>
+
+        <?php } ?>
     </main>
 
 </div>
