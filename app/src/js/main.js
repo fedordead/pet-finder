d665cf2f55e1b24d701299ac467359c758113c8f
Gary Wood <gwood2k9@gmail.com>
2016-05-18 15:07:23 +0100

    Added node list listener function

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -4,18 +4,11 @@ petDetectrApp.formSetup = function formSetup() {
     // Grab radio buttons and text nodes
     this.petStatus = document.querySelectorAll('[name="status"]');
     this.lastSeenText = document.querySelectorAll('.js-seen-found-text');
-    this.isChippedSelector = document.querySelectorAll('[name=is-chipped]');
+    this.isChippedSelector = document.querySelectorAll('[name=is-chipped] + label');
     this.chipNumber = document.querySelector('#chip-number-wrap');
-    
-    // add event listeners to radio buttons
-    for (let i = 0, max = this.petStatus.length; i < max; i++) {
-        this.petStatus[i].addEventListener('click', this.updateDateAndLocationText, true);
-    }
-    
-    // event listener for chip number toggle
-    for (let i = 0; i < this.isChippedSelector.length; i++) {
-        this.isChippedSelector[i].addEventListener('click', this.hideShowChipNumber, true);
-    }
+
+    this.addEventToNodes('click', this.petStatus, this.updateDateAndLocationText);
+    this.addEventToNodes('click', this.isChippedSelector, this.hideShowChipNumber);
 };
 
 petDetectrApp.updateDateAndLocationText = function updateDateAndLocationText(e) {
@@ -33,6 +26,13 @@ petDetectrApp.updateDateAndLocationText = function updateDateAndLocationText(e) 
     // updates all the text nodes
     for (let i = 0, max = petDetectrApp.lastSeenText.length; i < max; i++) {
         petDetectrApp.lastSeenText[i].innerHTML = lastSeenValue;
+    }
+};
+
+// Add listener to nodes
+petDetectrApp.addEventToNodes = function addEventToNodes(evt, nodes, func) {
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].addEventListener(evt, func, true);
     }
 };
 
@@ -56,7 +56,7 @@ petDetectrApp.setHideShow = function showHideElement(target, display) {
 petDetectrApp.hideShowChipNumber = function hideShowChipNumber(e) {
     const self = petDetectrApp;
     const isChipped = (e.target.getAttribute('for') === 'chipped') ? true : false;
-    
+
     if (isChipped) {
         self.setHideShow(self.chipNumber, true);
     } else {
