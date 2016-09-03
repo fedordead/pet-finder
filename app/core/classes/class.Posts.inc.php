92c128de21102ad4097526a07dc145c373afd9c2
Nick Moreton <nickmoreton@MacBook-Pro.local>
2016-09-03 16:48:25 +0100

    Style tweak

--- app/core/classes/class.Posts.inc.php
+++ app/core/classes/class.Posts.inc.php
@@ -22,8 +22,7 @@ class Posts {
             // set the PDO error mode to exception
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
-        catch(PDOException $e)
-            {
+        catch(PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
             }
     }
