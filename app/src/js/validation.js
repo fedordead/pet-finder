29c57b77ddf803952d13036cfa0b37fa2c6ad80f
DaveOrDead <davidajberner@gmail.com>
2016-09-10 22:45:50 +0100

    debounce working

--- app/src/js/validation.js
+++ app/src/js/validation.js
@@ -6,7 +6,6 @@ const updateResults = () => {
     const params = {};
 
     return e => {
-        console.log(e);
         params[e.target.name] = e.target.value;
         const query = objectKeyValuesToString(params, '=', '&');
 
@@ -19,35 +18,34 @@ const updateResults = () => {
     };
 };
 
-// adapted from _.underscore
+// adapted into es6 from the es5 _.underscore version
 const debounce = (func, wait, immediate) => {
     let timeout;
-
-    return () => {
-        console.log(...arguments);
+    return (...params) => {
         const later = () => {
             timeout = null;
             if (!immediate) {
-                func(...arguments);
+                func(...params);
             }
         };
 
         const callNow = immediate && !timeout;
+
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
 
         if (callNow) {
-            func(...arguments);
+            func(...params);
         }
     };
-};
+}
 
 const setupAjaxers = () => {
     const formFields = qa('select');
     const name = q('#pet-name');
     const handleChange = updateResults();
-    const handleTextChange = debounce(e => handleChange(e), 2000);
-    name.addEventListener('keyup', handleTextChange, true);
+    const handleTextChange = debounce(handleChange, 2000);
+    name.addEventListener('keyup', handleTextChange);
     addEventToNodes('change', formFields, handleChange);
 };
 
